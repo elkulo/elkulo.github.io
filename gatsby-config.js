b76e0292e46c58e2db20494c135c4076532fc934
A.Sudo <50855593+elkulo@users.noreply.github.com>
2021-10-24 17:05:00 +0900

    フォームURL変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
     social: {
       github: `elkulo`,
     },
-    formUrl: `https://forms.gle/Ra9gNBircfwg2umm9`,
+    formUrl: `https://inquiry.elkulo.me/elkulo_github_io/post`,
     robots: `noindex,nofollow`,
     verification: process.env.SEARCH_ID ? process.env.SEARCH_ID : "",
   },
