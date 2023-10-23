ce7aaa4e744c3e31fe4fa94fdef4ba22fd011d26
A.Sudo <50855593+elkulo@users.noreply.github.com>
2023-10-23 20:58:07 +0900

    Updates

---
+++ component---src-pages-404-js-158ac0f4dea8d8c8a872.js
@@ -0,0 +1,32 @@
+"use strict";(self.webpackChunkel_kulo=self.webpackChunkel_kulo||[]).push([[883],{4613:function(e,t,n){var a=n(4836);t.Z=void 0;var l=a(n(4938)),o=n(5893),i=(0,l.default)((0,o.jsx)("path",{d:"M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z"}),"KeyboardBackspace");t.Z=i},4126:function(e,t,n){n.r(t),n.d(t,{Head:function(){return u},default:function(){return s}});var a=n(7294),l=n(4854),o=n(8008),i=n(4613),r=n.p+"static/bg-404-4d97dda96b0dad0a88d4410fa795163d.jpg",c=n(932);const d={root:c.Z.div`
+    display: flex;
+    align-items: center;
+    min-height: var(--maxvh, 100vh);
+    background-image: url(${r});
+    background-position: 30% 50%;
+    background-size: cover;
+    color: #eee;
+    font-size: 10px;
+    text-shadow: 0 0 1px #000;
+  `,container:c.Z.div`
+    padding: 0 1em;
+    font-size: calc(100% + (1vw * 2.625));
+  `,branding:c.Z.p`
+    font-size: 0.875em;
+    font-weight: 600;
+    margin: 0;
+    padding: 0;
+  `,title:c.Z.h1`
+    letter-spacing: -0.02em;
+    font-size: 2em;
+    font-weight: 900;
+    color: #61d4e4;
+    margin: 0;
+  `,description:c.Z.p`
+    font-size: 0.875em;
+    font-weight: 600;
+  `,content:c.Z.div`
+    font-size: 0.675em;
+    font-weight: 400;
+  `};var s=e=>{let{data:t}=e;const n=t.site.siteMetadata.title;return a.createElement(d.root,null,a.createElement(d.container,null,a.createElement(d.branding,null,"Oops..."),a.createElement(d.title,null,"404 NOT FOUND"),a.createElement(d.description,null,"指定されたページが存在しません。"),a.createElement(d.content,null,a.createElement("p",null,"お探しのページは削除されたか、",a.createElement("br",null),"アドレスが間違っている可能性があります。"),a.createElement(i.Z,{style:{fontSize:"0.75em",color:"#b6bdf2"}})," ",a.createElement(l.Link,{to:"/",style:{fontSize:"0.875em",color:"#b6bdf2",boxShadow:"none"}},n," のホームに戻る"))))};const u=()=>a.createElement(o.Z,{title:"404 NOT FOUND"})}}]);
+//# sourceMappingURL=component---src-pages-404-js-158ac0f4dea8d8c8a872.js.map
