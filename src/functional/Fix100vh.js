80e2470e289fadfdb97869ee4c24d75163de2df1
A.Sudo <50855593+elkulo@users.noreply.github.com>
2023-12-19 20:27:03 +0900

    build

--- src/functional/Fix100vh.js
+++ src/functional/Fix100vh.js
@@ -4,9 +4,28 @@
  * @return {void}
  */
 const Fix100vh = () => {
-  document.documentElement.style.setProperty(
-    '--maxvh',
-    `${window.innerHeight}px`,
-  )
+  const timer = {
+    id: 0,
+    delay: 1,
+  }
+
+  const setStyleProperty = () => {
+    if (!timer.id) {
+      timer.id = setTimeout(
+        () => {
+          document.documentElement.style.setProperty(
+            '--maxvh',
+            `${window.innerHeight}px`,
+          )
+          clearTimeout(timer.id)
+          timer.id = 0
+        },
+        Math.floor(1000 * timer.delay),
+      )
+    }
+  }
+
+  window.addEventListener('resize', setStyleProperty, false)
+  window.addEventListener('DOMContentLoaded', setStyleProperty, false)
 }
 export default Fix100vh
