1bcccad0c2052c9bcab426884558ef275f7499bc
A.Sudo <50855593+elkulo@users.noreply.github.com>
2025-02-28 10:04:45 +0900

    Updates

---
+++ component---src-pages-404-js-facdc3047fccde7471b1.js
@@ -0,0 +1,32 @@
+"use strict";(self.webpackChunkel_kulo=self.webpackChunkel_kulo||[]).push([[125],{4172:function(e,t,n){n.r(t),n.d(t,{Head:function(){return f},default:function(){return u}});var l=n(6540),a=n(4794),o=n(2415),i=n(5003),r=n(4848),c=(0,i.A)((0,r.jsx)("path",{d:"M21 11H6.83l3.58-3.59L9 6l-6 6 6 6 1.41-1.41L6.83 13H21z"}),"KeyboardBackspace"),d=n.p+"static/bg-404-4d97dda96b0dad0a88d4410fa795163d.jpg",s=n(1479);const m={root:s.A.div`
+    display: flex;
+    align-items: center;
+    min-height: var(--maxvh, 100vh);
+    background-image: url(${d});
+    background-position: 30% 50%;
+    background-size: cover;
+    color: #eee;
+    font-size: 10px;
+    text-shadow: 0 0 1px #000;
+  `,container:s.A.div`
+    padding: 0 1em;
+    font-size: calc(100% + (1vw * 2.625));
+  `,branding:s.A.p`
+    font-size: 0.875em;
+    font-weight: 600;
+    margin: 0;
+    padding: 0;
+  `,title:s.A.h1`
+    letter-spacing: -0.02em;
+    font-size: 2em;
+    font-weight: 900;
+    color: #61d4e4;
+    margin: 0;
+  `,description:s.A.p`
+    font-size: 0.875em;
+    font-weight: 600;
+  `,content:s.A.div`
+    font-size: 0.675em;
+    font-weight: 400;
+  `};var u=e=>{let{data:t}=e;const n=t.site.siteMetadata.title;return l.createElement(m.root,null,l.createElement(m.container,null,l.createElement(m.branding,null,"Oops..."),l.createElement(m.title,null,"404 NOT FOUND"),l.createElement(m.description,null,"指定されたページが存在しません。"),l.createElement(m.content,null,l.createElement("p",null,"お探しのページは削除されたか、",l.createElement("br",null),"アドレスが間違っている可能性があります。"),l.createElement(c,{style:{fontSize:"0.75em",color:"#b6bdf2"}})," ",l.createElement(a.Link,{to:"/",style:{fontSize:"0.875em",color:"#b6bdf2",boxShadow:"none"}},n," のホームに戻る"))))};const f=()=>l.createElement(o.A,{title:"404 NOT FOUND"})}}]);
+//# sourceMappingURL=component---src-pages-404-js-facdc3047fccde7471b1.js.map
