b3bdfcb3f8763e77bdf055fdba9edf34066748ac
A.Sudo <50855593+elkulo@users.noreply.github.com>
2026-03-20 11:34:45 +0900

    Updates

---
+++ _astro/Image.C8gEVJwo.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app.CHkMmP-T.js";import"./index.BZLLzmcp.js";const e=({src:t,alt:i=""})=>t?o.jsx("img",{src:t,alt:i,style:{width:"100%",height:"auto",display:"block"},loading:"lazy"}):null;export{e as I};
